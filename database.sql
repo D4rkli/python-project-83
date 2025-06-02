0ff32db6e4f9cb834760f64e08da0236adec29f0
Anzhelika <darkl11n3ss@gmail.com>
2025-06-02 19:08:53 +0300

    chore: ignore .idea directory

--- database.sql
+++ database.sql
@@ -5,7 +5,6 @@ CREATE TABLE IF NOT EXISTS urls (Add commentMore actions
 );
 
 CREATE TABLE IF NOT EXISTS url_checks (
-@@ -11,5 +11,5 @@ CREATE TABLE IF NOT EXISTS url_checks (
     h1 TEXT,
     title TEXT,
     description TEXT,
